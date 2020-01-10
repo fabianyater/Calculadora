8c4af676a9cee5e93a5c97f425625107380a54bf
fabianyate31 <fabianyate31@gmail.com>
2020-01-09 23:04:28 -0500

    All files modified

--- main.js
+++ main.js
@@ -1,32 +1,16 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
 function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
-            return add(num1, num2);
+            return num1 + num2;
             break;
         case "-":
-            return subtract(num1, num2);
+            return num1 - num2;
             break;
         case "*":
-            return multiply(num1, num2);
+            return num1 * num2;
             break;
         case "/":
-            return divide(num1, num2);
+            return num1 / num2;
             break;
         default:
             return "Error. No valid operator."
