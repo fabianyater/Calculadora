91a3d2e01176a04f2ebb08868c20407be1ac4de8
Fabian Yate <fabianyater@users.noreply.github.com>
2020-01-13 05:39:24 -0500

    Update main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -59,7 +59,7 @@ function calculate() {
     secondNumber = +view.textContent;
 (view.textContent.length > 10)? view.style.fontSize = "1.2em": view.style.fontSize = "2em";
     view.innerHTML = operate(operator, firstNumber, secondNumber);
-    view.textContent += firstNumber + ' ' + secondNumber + ' =';
+    viewUp.textContent += firstNumber + ' ' + secondNumber + ' =';
     firstNumber = null;
     secondNumber = null;
 }
